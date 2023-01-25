5a203b05f3f82c78a2a6d998ae490b3165f0501c
pankajsingh22 <ps064876@gmail.com>
2023-01-25 14:32:53 +0530

    al

--- src/components/OurProducts.jsx
+++ src/components/OurProducts.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import { Container } from "react-bootstrap";
 
 const OurProducts = () => {
   return (
     <>
-    
+      <div>
+        <Container>
+          <div className=" max_w_780px">
+            <h2 className=" ff_gilroy fw-bold fs_xl tx_neutral">
+              Our products
+            </h2>
+          </div>
+        </Container>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default OurProducts
+export default OurProducts;
