9256f5ba1f577c11ccdce7b178aac2998e1363b0
aman91458 <aman91458@gmail.com>
2023-01-25 14:27:31 +0530

    al

--- src/App.js
+++ src/App.js
@@ -1,31 +1,9 @@
 import logo from "./logo.svg";
-<<<<<<< HEAD
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-=======
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
->>>>>>> 2940e266e847175f179e78e9a7209a6c7b3f8892
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <></>;
 }
 
 export default App;
