c112a059a04b91eb94da30a2c62e453f09149e0e
aman91458 <aman91458@gmail.com>
2023-01-25 14:44:21 +0530

    al

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Faq from "./components/Faq";
--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -1,10 +1,21 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
+import faqHand from "../assets/images/png/faqHand.png";
+import faqLeftPos from "../assets/images/png/faqLeftPos.png";
 
 const Faq = () => {
   return (
     <div>
-      <Container></Container>
+      <Container>
+        <Row>
+          <Col lg="6">
+            <div className=" position-relative">
+              <img className=" position-absolute" src={faqHand} alt="faqHand" />
+              <img src={faqLeftPos} alt="faqLeftPos" />
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
